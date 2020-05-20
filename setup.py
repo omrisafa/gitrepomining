ba7575a5e61943f362906c96a7464589ce994b98
Safa Omri <safaomri@Safas-MBP.fritz.box>
2020-05-20 19:27:36 +0200

    update setup

--- setup.py
+++ setup.py
@@ -3,31 +3,21 @@ from setuptools import setup, find_packages
 with open('requirement.txt') as reqs_file:
     requirements = reqs_file.read().splitlines()
 
-with open('test-requirements.txt') as reqs_file:
-    test_requirements = reqs_file.read().splitlines()
-
-long_description = 'pygitminer is a Python framework for Git Repository Mining.'
-
 setup(
     name='pygitminer',
-    description='Python Framework for Git Repository Mining',
-    long_description=long_description,
+    description='Git Repository Mining: a Python Framework',
     author='Safa Omri',
     author_email='safa.omri@kit.edu',
     version='1.15',
     packages=find_packages('.'),
     url='https://github.com/omrisafa/gitrepomining',
-    license='Apache License',
     package_dir={'pygitminer': 'pygitminer'},
     python_requires='>=3.5',
     install_requires=requirements,
-    test_requirements=requirements + test_requirements,
     classifiers=[
             'Development Status :: 4 - Beta',
             'Environment :: Console',
-            'Intended Audience :: Developers',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: Apache Software License',
+            'Intended Audience :: Research',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
